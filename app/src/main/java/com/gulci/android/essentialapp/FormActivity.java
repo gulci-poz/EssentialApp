a238ed6e5677ac8fb88a09e4ed3462fa27388a76
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2015-07-02 13:06:31 +0200

    2015-07-02 debug info

--- app/src/main/java/com/gulci/android/essentialapp/FormActivity.java
+++ app/src/main/java/com/gulci/android/essentialapp/FormActivity.java
@@ -1,23 +1,81 @@
 package com.gulci.android.essentialapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class FormActivity extends ActionBarActivity {
+public class FormActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = "FormActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        Log.d("FormActivity", "onCreate gulci log");
+
+        Button btn = (Button) findViewById(R.id.btnRegister);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "onClick: register button clicked");
+            }
+        });
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Log.d(LOG_TAG, "Changed to portrait");
+        } else {
+            Log.d(LOG_TAG, "Changed to landscape");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(LOG_TAG, "onStart gulci log");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(LOG_TAG, "onResume gulci log");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(LOG_TAG, "onPause gulci log");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(LOG_TAG, "onStop gulci log");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_form, menu);
+
+        Log.d("FormActivity", "onCreateOptionsMenu gulci log");
+
         return true;
     }
 
@@ -26,6 +84,9 @@ public class FormActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        Log.d("FormActivity", "onOptionsItemSelected gulci log");
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -35,4 +96,16 @@ public class FormActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*
+
+    //android:onClick="registerClickHandler"
+
+    public void registerClickHandler(View view) {
+
+        Button btn = (Button) view;
+        Log.d("FormActivity", "registerClickHandler: " + btn.getText());
+    }
+
+    */
 }
